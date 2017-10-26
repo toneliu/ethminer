c3d8fd50d13d888427ccd4682fec6a4f3b701c8b
toneliu <olhto@qq.com>
2017-10-26 15:33:29 +0000

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./ethminer -U -F http://eth-cn2.dwarfpool.com:8000/0xfC820096d4439Da123DEd8fd6F224F8d587487e3/worker1  -SP 1  --farm-recheck 200 --cuda-block-size 700 --cuda-grid-size 16384
+./ethminer -U -F http://eth-cn2.dwarfpool.com:8000/0xfC820096d4439Da123DEd8fd6F224F8d587487e3/worker1  -SP 1  --farm-recheck 200 --cuda-block-size 700 --cuda-grid-size 30000
